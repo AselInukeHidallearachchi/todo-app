d0bda6a8f4e0c65a8613417d8273c993b6fc547d
Asel Hidallearachchi <aselinuke@gmail.com>
2025-12-01 14:06:08 +0530

    chore: remove comments

--- api/app/Console/Commands/SendTaskDigests.php
+++ api/app/Console/Commands/SendTaskDigests.php
@@ -11,18 +11,10 @@ use Illuminate\Support\Facades\Mail;
 
 class SendTaskDigests extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'tasks:send-digests';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+
     protected $description = 'Send daily task digests to users at their chosen time';
 
     /**
@@ -33,7 +25,7 @@ class SendTaskDigests extends Command
         info("Task digest command is executing");
         $now = Carbon::now(); // Uses APP_TIMEZONE (e.g. Asia/Colombo)
 
-        //logging the server time. 
+        //logging the server time.
         info('current time'.$now);
 
         $windowStart = $now->copy()->subMinutes(2)->format('H:i:s');
@@ -47,13 +39,13 @@ class SendTaskDigests extends Command
 
         /*
         // TESTING CODE (used to send to all enabled users regardless of time)
-        // Commented out above and uncomment below for testing. 
+        // Commented out above and uncomment below for testing.
         // */
-        
+
         // $users = User::whereHas('preference', function ($q) {
         //     $q->where('daily_digest_enabled', true);
         // })->get();
-        
+
 
         foreach ($users as $user) {
             // Prevent double-send for the same day COMMENT TO TEST
